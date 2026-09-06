29637fcf93c6ba96bb15e489346e59536df6427a
Abhishek <abhishekmondeddula6@gmail.com>
2026-09-06 16:47:27 +0530

    Modules

---
+++ DAY20/main.py
@@ -0,0 +1,15 @@
+import math
+import random
+import my_module
+
+
+number = 25
+
+print("Square Root:", math.sqrt(number))
+
+print("Random Number:", random.randint(1, 100))
+
+print(my_module.greet("Python Learner"))
+
+result = my_module.add(10, 20)
+print("Addition:", result)
