8360888f6e67e8b76427be34c2a870ab8f5d7eac
Abhishek <abhishekmondeddula6@gmail.com>
2026-09-06 16:43:00 +0530

    Recursion

---
+++ DAY18/recursion.py
@@ -0,0 +1,29 @@
+def factorial(n):
+    # Base case
+    if n == 0 or n == 1:
+        return 1
+    # Recursive case
+    else:
+        return n * factorial(n - 1)
+
+print(factorial(5))  # Output: 120
+
+
+
+
+
+
+
+
+def fibonacci(n):
+    # Base cases
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    # Recursive case
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+for i in range(6):
+    print(fibonacci(i), end=" ")  # Output: 0 1 1 2 3 5
